64420a66d9769b3c504d0415e16c3afc8d0873ad
keecth <gusarov.du@gmail.ocm>
2024-03-17 01:30:00 +0300

    Реализовал нажатие кнопок и сохранение данных в БД

--- keyboards/joystick.py
+++ keyboards/joystick.py
@@ -4,9 +4,9 @@ from aiogram.utils.keyboard import InlineKeyboardBuilder
 router = Router()
 def get_specs():
     kb=InlineKeyboardBuilder()
-    kb.button(text="Стрелок", callback_data="Rifler")
-    kb.button(text="Минометчик", callback_data="Artillery")
-    kb.button(text="Связист", callback_data="Operator")
-    kb.button(text="Инженер", callback_data="Engineer")
+    kb.button(text="Стрелок", callback_data=0)
+    kb.button(text="Минометчик", callback_data=1)
+    kb.button(text="Связист", callback_data=2)
+    kb.button(text="Инженер", callback_data=3)
     kb.adjust(1)
     return kb.as_markup()
